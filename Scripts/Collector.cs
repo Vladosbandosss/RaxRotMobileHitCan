53e96ec680df7c1864c664e5401f4a29c54b4afd
Vladosbandosss <dasistperfektos@gmail.com>
2021-11-08 19:40:50 +0300

    ReadyPrototype
    RG

---
+++ Scripts/Collector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collector : MonoBehaviour
+{
+    public int cansCount;
+    public GameObject bal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cans")
+        {
+            other.gameObject.SetActive(false);
+            cansCount--;
+
+            if (cansCount == 0)
+            {
+                Invoke(nameof(RestartGame), 2f);
+            }
+        }
+
+        if (other.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+
+            if (cansCount != 0)
+            {
+                Instantiate(bal);
+            }
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+}
---
+++ Scripts/SwipeBal.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeBal : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    private float touchStart;
+    private float touchFinish;
+
+    Vector3 throwForce;
+
+    private Rigidbody rb;
+
+    private AudioSource audioFx;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioFx = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)||Input.touchCount>0&&Input.GetTouch(0).phase==TouchPhase.Began)
+        {
+            touchStart = Time.time;
+            startPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            touchFinish = Time.time;
+            endPos = Input.mousePosition;
+
+            float totalTime = touchFinish - touchStart;
+
+            Vector3 distanse = endPos - startPos;
+            distanse.z = distanse.magnitude;
+
+            throwForce = distanse / totalTime;
+
+
+            rb.AddForce(throwForce/3f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cans")
+        {
+            audioFx.Play();
+        }
+    }
+}
